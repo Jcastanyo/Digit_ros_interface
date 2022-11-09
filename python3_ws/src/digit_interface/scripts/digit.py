fcaf016493226a5f9d573972e1e05af730e06d68
Jcastanyo <jcarobotica@gmail.com>
2022-11-09 09:37:00 +0100

    two sensors functionality

--- python3_ws/src/digit_interface/scripts/digit.py
+++ python3_ws/src/digit_interface/scripts/digit.py
@@ -7,6 +7,7 @@ import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import time
+import cv2
 
 # DIGIT LIBRARY
 from digit_interface import Digit
@@ -21,12 +22,15 @@ def setup_digit():
 
     # get sensor id from launch file
     id = rospy.get_param("/sensor_id")
+    id55, id45 = id.split("_")[0], id.split("_")[1]
 
     # initialize digit sensor with the usb serial
-    d = Digit(id)
-    d.connect()
+    d55 = Digit(id55)
+    d45 = Digit(id45)
+    d55.connect()
+    d45.connect()
 
-    return d
+    return d55, d45, id55, id45
 
 
 '''
@@ -39,12 +43,13 @@ def setup_digit():
 def main():
 
     # initialize digit sensor
-    digit_sensor = setup_digit()
+    d55, d45, id55, id45 = setup_digit()
 
     # initialize ros node
-    rospy.init_node("digit_ros_node", anonymous=True)
+    rospy.init_node(f"digit_ros_node", anonymous=True)
     # create publisher with the topic and the type of the msg
-    pub = rospy.Publisher("digit/camera/image_color", Image, queue_size=10)
+    pub55 = rospy.Publisher(f"digit55/camera/image_color", Image, queue_size=10)
+    pub45 = rospy.Publisher(f"digit45/camera/image_color", Image, queue_size=10)
 
     # cvbridge to transform the images from np to ros type
     cvbridge = CvBridge()
@@ -56,22 +61,37 @@ def main():
         rospy.loginfo("Publishing Digit images!")
 
         # get image from the sensor
-        digit_img = digit_sensor.get_frame()
+        digit55_img = d55.get_frame()
+        digit45_img = d45.get_frame()
+
+        cv2.imshow("digit55", digit55_img)
+        cv2.imshow("digit45", digit45_img)
+        cv2.waitKey(1)
 
         # create the ros msg for the images and fill with all the info
-        image_msg = Image()
-        image_msg.header.stamp = rospy.Time.from_sec(time.time())
-        image_msg.header.frame_id = "digit_camera"
-        image_msg.height = (digit_img.shape)[0]
-        image_msg.width = (digit_img.shape)[1]
-        image_msg.step = digit_img.strides[0]
-        image_msg.data = digit_img.flatten().tolist()
-        image_msg.encoding = "bgr8"
+        image_msg55 = Image()
+        image_msg55.header.stamp = rospy.Time.from_sec(time.time())
+        image_msg55.header.frame_id = "digit_camera"
+        image_msg55.height = (digit55_img.shape)[0]
+        image_msg55.width = (digit55_img.shape)[1]
+        image_msg55.step = digit55_img.strides[0]
+        image_msg55.data = digit55_img.flatten().tolist()
+        image_msg55.encoding = "bgr8"
 
+        
+        image_msg45 = Image()
+        image_msg45.header.stamp = rospy.Time.from_sec(time.time())
+        image_msg45.header.frame_id = "digit_camera"
+        image_msg45.height = (digit45_img.shape)[0]
+        image_msg45.width = (digit45_img.shape)[1]
+        image_msg45.step = digit45_img.strides[0]
+        image_msg45.data = digit45_img.flatten().tolist()
+        image_msg45.encoding = "bgr8"
 
         # transform to ros type and publish img
         #pub.publish(cvbridge.cv2_to_imgmsg(digit_img))
-        pub.publish(image_msg)
+        pub55.publish(image_msg55)
+        pub45.publish(image_msg45)
         # 100 hz rate of publication
         rospy.Rate(100)
 
